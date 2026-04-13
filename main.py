8f495e767d835363068d742231f4c548b07d07c4
Gaurav Bansode <gauravraiwat123@gmail.com>
2026-04-14 00:16:18 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,40 @@
+from fastapi import FastAPI
+from pydantic import BaseModel
+from pymongo import MongoClient
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # allow all (for now)
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+# Connect to MongoDB
+client = MongoClient("mongodb+srv://admin:admin123@cluster.cymziw9.mongodb.net/?appName=Cluster")
+db = client["exam_prep"]
+users_collection = db["users"]
+
+# Model
+class User(BaseModel):
+    name: str
+    email: str
+    college: str
+    year: str
+
+# Signup API
+@app.post("/signup")
+def signup(user: User):
+    users_collection.insert_one(user.dict())
+    return {"message": "User saved"}
+
+# Count API
+@app.get("/count")
+def get_count():
+    count = users_collection.count_documents({})
+    return {"count": count}
