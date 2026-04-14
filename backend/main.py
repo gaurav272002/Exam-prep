bf82c603af462f5f9706fd53a27b106d53c730f7
Gaurav Bansode <gauravraiwat123@gmail.com>
2026-04-14 17:58:57 +0530

    changes

--- backend/main.py
+++ backend/main.py
@@ -112,7 +112,7 @@ Text:
 """
     print("In Ai ")
     response = requests.post(
-        "https://openrouter.ai/api/v1/chat/completions",
+        # "https://openrouter.ai/api/v1/chat/completions",
         headers={
             "Authorization": "Bearer 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",
             "Content-Type": "application/json"
